878022392f5ca3b874f587889e303cda643e3601
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-06 12:02:11 +0300

    Add project files.

---
+++ StocksApp/Services/MyService.cs
@@ -0,0 +1,26 @@
+﻿namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public MyService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task Method()
+        {
+            using(var httpClient = httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new()
+                {
+                    RequestUri = new Uri("url"),
+                    Method = HttpMethod.Get,
+                };
+
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            }
+        }
+    }
+}
