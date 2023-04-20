db3d9260ff333f9fd7e36b0d9ab011637e508d2b
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-20 15:36:04 +0300

    Refactored unit tests to use Moq, Fixture, FluentAssertions and added controller unit test

--- ServiceContracts/DTO/SellOrderRequest.cs
+++ ServiceContracts/DTO/SellOrderRequest.cs
@@ -3,13 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ServiceContracts.DTO
 {
-    public class SellOrderRequest
+    public class SellOrderRequest : IValidatableObject
     {
         [Required]
         public string StockSymbol { get; set; }
         [Required]
         public string StockName { get; set; }
-        [Range(typeof(DateTime), "1/1/2000", "31/12/9999", ErrorMessage = "The date must be after 1/1/2000")]
         public DateTime DateAndTimeOfOrder { get; set; }
         [Range(1, 100000)]
         public uint Quantity { get; set; }
@@ -27,6 +26,19 @@ namespace ServiceContracts.DTO
                 Quantity = Quantity,
                 Price = Price
             };
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> results = new List<ValidationResult>();
+
+            //Date of order should be later than Jan 01, 2000
+            if (DateAndTimeOfOrder < Convert.ToDateTime("2000-01-01"))
+            {
+                results.Add(new ValidationResult("Date of the order should not be older than Jan 01, 2000."));
+            }
+
+            return results;
         }
     }
 }
--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -1,1 +1,4 @@
 global using Xunit;
+global using Moq;
+global using AutoFixture;
+global using FluentAssertions;
